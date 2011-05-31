f17a70765679a409384d57a1d2a0f8c56c619468
ggrest87 <ggrest87@gmail.com>
2011-05-31 14:17:54 +0000

    New classes

---
+++ src/Ray.cpp
@@ -0,0 +1,24 @@
+/*
+ * Rayon.cpp
+ *
+ *  Created on: 31.05.2011
+ *      Author: nb-gyglim
+ */
+
+#include "Ray.h"
+
+
+Ray::Ray(Vector3 p1, Vector3 p2) {
+	ptO = p1;
+	ptD = p2;
+
+}
+
+	//returns the point on the ray corresponding t
+	Vector3 Ray::getPoint(double t) {
+		return ptO + ptD*t;
+	}
+
+Ray::~Ray() {
+	// TODO Auto-generated destructor stub
+}
---
+++ src/Vector3.cpp
@@ -0,0 +1,32 @@
+/*
+ * Vecteur3.cpp
+ *
+ *  Created on: 31.05.2011
+ *      Author: nb-gyglim
+ */
+
+#include "Vector3.h"
+
+Vector3::Vector3(double x, double y, double z) {
+	coords[0] = x;
+	coords[1] = y;
+	coords[2] = z;
+
+}
+
+Vector3 Vector3::operator*(double s) {
+	Vector3 strechedVector = Vector3(coords[0]*s,coords[1]*s,coords[2]*s);
+
+	return strechedVector;
+}
+
+Vector3 Vector3::operator+(Vector3 v) {
+	Vector3 addedVector = Vector3(coords[0]* v.coords[0],coords[1]* v.coords[1],coords[2]* v.coords[2]);
+
+	return addedVector;
+}
+
+
+Vector3::~Vector3() {
+	// TODO Auto-generated destructor stub
+}
