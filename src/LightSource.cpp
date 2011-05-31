6c059f22fb5d1caf2f546a9e94bbfb64dd2bb3d7
ggrest87 <ggrest87@gmail.com>
2011-05-31 15:50:36 +0000

    New classes

---
+++ src/LightSource.cpp
@@ -0,0 +1,18 @@
+/*
+ * LightSource.cpp
+ *
+ *  Created on: 31.05.2011
+ *      Author: nb-gyglim
+ */
+
+#include "LightSource.h"
+
+LightSource::LightSource(double intens/*, Vector3 pos*/) {
+	intensity = intens;
+	//position = pos;
+
+}
+
+LightSource::~LightSource() {
+	// TODO Auto-generated destructor stub
+}
