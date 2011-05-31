851f6fbf0a3594b44dfb8c708ccfc81c7221ad6a
quentin.grimaud <quentin.grimaud@gmail.com>
2011-05-31 16:53:09 +0000

    debut implementation lancer de rayons

--- src/application.cpp
+++ src/application.cpp
@@ -13,6 +13,15 @@ using namespace std;
 int main ( int argc, char **argv )
 {
 	cout << "test huhu" << endl;
+
+	Vector3 obs ( 0.0, 0.0, 0.0 );
+	Vector3 aimedPoint ( 0.0, 10.0, 0.0 );
+	double distScreen = 2.0;
+	double Rx = 400.0;
+	double Ry = 300.0;
+	Screen s = Screen ( obs, aimedPoint, distScreen, Rx, Ry );
+	// TODO: lancer le rayon
+
 	return 0;
 } // main ()
 
