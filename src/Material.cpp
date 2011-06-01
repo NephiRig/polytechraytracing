b35283873a0cf9691f0ed8686d7a24f9dc9a7e37
ggrest87 <ggrest87@gmail.com>
2011-06-01 09:55:13 +0000

    New classes

---
+++ src/Material.cpp
@@ -0,0 +1,17 @@
+/*
+ * Material.cpp
+ *
+ *  Created on: 01.06.2011
+ *      Author: nb-gyglim
+ */
+
+#include "Material.h"
+
+Material::Material() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Material::~Material() {
+	// TODO Auto-generated destructor stub
+}
