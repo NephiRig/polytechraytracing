9c498f0d7e0dade5abb7f779f43f7dcd65667832
ggrest87@gmail.com <ggrest87@gmail.com@f53c08bf-0994-eb6e-57c7-fd235582c9a5>
2011-06-01 09:55:13 +0000

    New classes

---
+++ trunk/src/Material.h
@@ -0,0 +1,17 @@
+/*
+ * Material.h
+ *
+ *  Created on: 01.06.2011
+ *      Author: nb-gyglim
+ */
+
+#ifndef MATERIAL_H_
+#define MATERIAL_H_
+
+class Material {
+public:
+	Material();
+	virtual ~Material();
+};
+
+#endif /* MATERIAL_H_ */
