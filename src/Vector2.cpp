74e8769a6f97860459a42e9aa5d52d3f3b5ed9d7
shaolan60 <shaolan60@gmail.com>
2011-06-04 10:26:46 +0000

    Classe Vector2 implémentée

--- src/Vector2.cpp
+++ src/Vector2.cpp
@@ -2,7 +2,7 @@
  * Vecteur2.cpp
  *
  *  Created on: 31.05.2011
- *      Author: nb-gyglim
+ *      Author: nb-gyglim, Michelet Valentin
  */
 
 #include "Vector2.h"
@@ -13,13 +13,116 @@ Vector2::Vector2(double x, double y) {
 	coords[1] = y;
 }
 
+Vector2::Vector2(const Vector2 &v1) {
+	*this = v1;
+}
+
 Vector2::~Vector2() {
 	// TODO Auto-generated destructor stub
 }
 
-double Vector2::norm ()
-{
-	return sqrt ( coords[0]*coords[0] + coords[1]*coords[1] );
+
+double Vector2::get_x() const {
+	return coords[0];
+}
+
+double Vector2::get_y() const {
+	return coords[1];
+}
+
+
+void Vector2::set_x(double x) {
+	coords[0] = x;
+}
+
+void Vector2::set_y(double y) {
+	coords[1] = y;
+}
+
+
+double Vector2::operator[](int i) const {
+	return coords[i];
+}
+
+double& Vector2::operator[](const int &i) {
+	return coords[i];
+}
+
+
+bool operator==(const Vector2 &v1, const Vector2 &v2) {
+	return v1[0] == v2[0] && v1[1] == v2[1];
+}
+
+bool operator!=(const Vector2 &v1, const Vector2 &v2) {
+	return !(v1 == v2);
+}
+
+
+const Vector2& Vector2::operator+() const {
+	return *this;
+}
+
+Vector2 Vector2::operator-() const {
+	return Vector2(-coords[0], -coords[1]);
+}
+
+
+Vector2& Vector2::operator=(const Vector2 &v) {
+	coords[0] = v[0];
+	coords[1] = v[1];
+	return *this;
+}
+
+
+Vector2 operator*(const Vector2& v, const double k) {
+	return Vector2(v[0] * k, v[1] * k);
+}
+
+Vector2 operator*(const double k, const Vector2& v) {
+	return Vector2(v[0] * k, v[1] * k);
+}
+
+Vector2 operator/(const Vector2& v, const double k) {
+	return Vector2(v[0] / k, v[1] / k);
+}
+
+Vector2 operator+(const Vector2& v1, const Vector2& v2) {
+	return Vector2(v1[0] + v2[0], v1[1] + v2[1]);
+}
+
+Vector2 operator-(const Vector2& v1, const Vector2& v2) {
+	return Vector2(v1[0] - v2[0], v1[1] - v2[1]);
+}
+
+
+Vector2& Vector2::operator*=(const double k) {
+	return *this = (*this * k);
+}
+
+Vector2& Vector2::operator/=(const double k) {
+	return *this = (*this / k);
+}
+
+Vector2& Vector2::operator+=(const Vector2& v) {
+	return *this = (*this + v);
+}
+
+Vector2& Vector2::operator-=(const Vector2& v) {
+	return *this = (*this - v);
+}
+
+
+double dot_product(const Vector2& v1, const Vector2& v2) {
+	return v1[0] * v2[0] + v1[1] * v2[1];
+}
+
+
+double Vector2::norm () const {
+	return sqrt(coords[0]*coords[0] + coords[1]*coords[1]);
+}
+
+double Vector2::squared_norm() const {
+	return (coords[0] * coords[0] + coords[1] * coords[1]);
 }
 
 Vector2 Vector2::normalized ()
@@ -38,7 +141,15 @@ void Vector2::normalize ()
 	coords[1] *= ni;
 }
 
-double& Vector2::operator[] ( const int& i )
-{
-	return coords[i];
+Vector2 unitVector(const Vector2 &v) {
+	return v / v.norm();
+}
+
+
+ostream& operator<<(ostream &os, const Vector2 &v) {
+	return os << "(" << v[0] << "; " << v[1] << ")";
+}
+
+istream& operator>>(istream &is, Vector2 &v) {
+	return is >> v.coords[0] >> v.coords[1];
 }
