6d3066cf0903269af50a784346bf201620503cb7
shaolan60 <shaolan60@gmail.com>
2011-06-05 12:16:24 +0000

    Classe Ray minimale complétée

--- src/Ray.h
+++ src/Ray.h
@@ -2,24 +2,45 @@
  * Rayon.h
  *
  *  Created on: 31.05.2011
- *      Author: nb-gyglim
+ *      Author: nb-gyglim, Michelet Valentin
  */
 
 #ifndef RAYON_H_
 #define RAYON_H_
 
 #include "Vector3.h"
+#include <iostream>
+
+using namespace std;
 
 class Ray {
 
 public:
-	Ray(Vector3 p1, Vector3 p2);
-	virtual ~Ray();
-
-	Vector3 getPoint(double t) const;
-
 	Vector3 ptD;
 	Vector3 ptO;
+
+	Ray(const Vector3 &v1 = Vector3(), const Vector3 &v2 = Vector3());
+	Ray(const Ray &r);
+	virtual ~Ray();
+
+	Vector3 get_ptD() const;
+	Vector3 get_ptO() const;
+
+	void setptD(const Vector3 &v_ptD);
+	void setptO(const Vector3 &v_ptO);
+
+	/**
+	 * \fn Vector3 getPoint(double t) const
+	 * \brief Value in a point
+	 *
+	 * Calculates the point of the ray at parameter t
+	 *
+	 * \param t: the parameter
+	 * \return the point of the ray at parameter t
+	 */
+	Vector3 getPoint(double t) const;
+
+	friend ostream& operator<<(ostream &os, const Ray &v);
 };
 
 #endif /* RAYON_H_ */
