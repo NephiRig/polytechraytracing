81774acf37318c1800d997e2a045305ab023404e
shaolan60@gmail.com <shaolan60@gmail.com@f53c08bf-0994-eb6e-57c7-fd235582c9a5>
2011-06-05 12:16:24 +0000

    Classe Ray minimale complétée

--- trunk/src/Ray.cpp
+++ trunk/src/Ray.cpp
@@ -2,23 +2,47 @@
  * Rayon.cpp
  *
  *  Created on: 31.05.2011
- *      Author: nb-gyglim
+ *      Author: nb-gyglim, Michelet Valentin
  */
 
 #include "Ray.h"
 
-
-Ray::Ray(Vector3 p1, Vector3 p2) {
+Ray::Ray(const Vector3 &p1, const Vector3 &p2) {
 	ptO = p1;
 	ptD = p2;
-
 }
 
-	//returns the point on the ray corresponding t
-	Vector3 Ray::getPoint(double t) const {
-		return ptO + (ptD*t);
-	}
+Ray::Ray(const Ray &r) {
+	*this = r;
+}
 
 Ray::~Ray() {
 	// TODO Auto-generated destructor stub
+}
+
+
+Vector3 Ray::get_ptD() const {
+	return ptD;
+}
+
+Vector3 Ray::get_ptO() const {
+	return ptO;
+}
+
+void Ray::setptD(const Vector3 &v_ptD) {
+	ptD = v_ptD;
+}
+
+void Ray::setptO(const Vector3 &v_ptO) {
+	ptO = v_ptO;
+}
+
+
+Vector3 Ray::getPoint(double t) const {
+	return ptO + (ptD * t);
+}
+
+
+ostream& operator<<(ostream &os, const Ray &v) {
+	return os << v.get_ptO() << " + t*" << v.get_ptO() << endl;
 }
