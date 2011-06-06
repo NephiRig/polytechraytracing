cda301271563f58517d7da2e0f3b54f8436f448e
ggrest87 <ggrest87@gmail.com>
2011-06-06 13:39:22 +0000

    Fixed intersect code. --> PLEASE DONT TOUCH!!!! Set: added function get (operator[] does not work work for me!) --> PLEASE LEAVE IT THERE

--- src/Sphere.cpp
+++ src/Sphere.cpp
@@ -20,28 +20,7 @@ Sphere::~Sphere() {
 }
 
 bool Sphere::intersect(const Ray &r, float tmin, float tmax/*, HitRecord *record*/) const {
-	/*Vector3 o_c = r[0] - centre;
-	double a = dot_product(r[1], r[0]);
-	double b = (dot_product(r[1], o_c));
-	double c = dot_product(r[0] - centre, r[0] - centre);
-	double d = b * b - a * c;
 
-	if (d < 0) {
-		double t = (-bp - sqrt(d)) / a;
-
-		if (t < tmin)
-			t = (-bp + sqrt(d)) / a;
-
-		if (t < tmin || t > tmax)
-			return false;
-		/*
-		record->t = t;
-		record->normal = unitVector(r.ptO + t * r.ptD - centre);
-		record->color = color;
-		//*/
-	/*
-		return true;
-	}//*/
 	return false;
 }
 
@@ -52,26 +31,38 @@ Set<Vector3> Sphere::intersect(const Ray& r) const {
 
 	Set<Vector3> s = Set<Vector3> (2);
 
-	Vector3 o_c = r[0] - centre;
-	double a = dot_product(r[1], r[0]);
-	double b = 2 * (dot_product(r[1], o_c));
-	//TODO real function for pow
-	double c = dot_product(r[0] - centre, r[0] - centre);
+	double a = r.get_direction().norm() * r.get_direction().norm();
 
-	//Apply solve function (-b +- sqrt(b²-4ac))/2a
+	Vector3 d_o_x = dot_product(r.get_direction(),(r.get_origin() - centre));
+	double b = 2 * (d_o_x[0] + d_o_x[1] + d_o_x[2]);
+	cout << " a=";
+	cout << a;
+	cout << " b=";
+	cout << b;
+
+	Vector3 o_c = dot_product(r.get_origin(),centre);
+	//TODO real function for pow
+	double c = centre.norm() * centre.norm() + r.get_origin().norm() * r.get_origin().norm() - 2
+			* (o_c[0] + o_c[1] + o_c[2]) - radius * radius;
+
+	cout << " c=";
+	cout << c;
+
+	//Apply solve function (-b +- sqrt(b^2-4ac))/2a
 	double d = b * b - 4 * a * c; //the term in the root
 
-	cout << a << " " << b << " " << c << " " << o_c << endl;
 
 	if (d < 0) { //We have no intersection
-		/*
 		Vector3 null = Vector3(0, 0, 0);
 		s.add(null); //FIXME return specific null value
-		//*/
 	} else {
-		cout << d << endl;
 		double t1 = (-b - sqrt(d)) / (2 * a);
 		double t2 = (-b + sqrt(d)) / (2 * a);
+		cout << " t1=";
+		cout << t1;
+		cout << " t2=";
+		cout << t2;
+
 		s.add(r.getPoint(t1));
 		if (t2 != t1) {
 			s.add(r.getPoint(t2));
