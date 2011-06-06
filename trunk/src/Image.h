f221813f1fa3f64eddf0656fc03202f3fe23211f
shaolan60@gmail.com <shaolan60@gmail.com@f53c08bf-0994-eb6e-57c7-fd235582c9a5>
2011-06-06 09:42:28 +0000

    Modification de la classe Image + méthode readPPM implémentée

--- trunk/src/Image.h
+++ trunk/src/Image.h
@@ -6,55 +6,28 @@
 #include <cstdlib>
 #include <iostream>
 
-class Image  
-{
+using namespace std;
+
+class Image {
 public:
+	Image() {}
+	Image(int _w, int _h);
+	Image(int _w, int _h, Color c);
 
-   Image() {}
-   Image(int _w, int _h);
-   Image(int _w, int _h , Color c);
-   void safeSetPixel(int x, int y, Color c)
-   {
-      if (x >= 0 && x < w && y >= 0 && y < h)
-         data[x][y] = c; 
-   }
-   void setPixel(int x, int y, Color c)
-   {
-      if (!(x >= 0 && x < w && y >= 0 && y < h))
-      {
-         std::cerr << "Image::setPixel    w = " << w << " h = " << h
-              << "     requested pixel (" << x << ", " << y << ")" 
-              << std::endl;
-         exit(0);
-      }
-      data[x][y] = c;
-   }
+	int width() const;
+	int height() const;
 
-   Color getPixel(int x, int y) const
-   {
-      x = ( x >= 0 ? x : 0);  
-      x = ( x <  w ? x : w-1);  
-      y = ( y >= 0 ? y : 0);  
-      y = ( y <  h ? y : h-1);  
-/*      if (!(x >= 0 && x < w && y >= 0 && y < h))
-      {
-         cerr << "Image::getPixel w = " << w << " h = " << h << endl
-            << "       requested pixel (" << x << ", " << y << ")" 
-            << endl;
-         exit(0);
-      }
-*/      
-      return data[x][y]; 
-   }
-   int width()  const { return w; }
-   int height() const { return h; }
-   //void readPPM (char* file_name);
-   void writePPM(std::ostream& s) const;
+	void safeSetPixel(int x, int y, Color c);
+	void setPixel(int x, int y, Color c);
+
+	Color getPixel(int x, int y) const;
+
+	void writePPM(ostream &s) const;
+	void readPPM (string file_name);
 
 private:
-   Color **data;
-   int w, h;
+	Color **data;
+	int w, h;
 };
-
 
 #endif
