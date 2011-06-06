2ce7e084de3a052206fcaaeb315a8b3908089515
shaolan60 <shaolan60@gmail.com>
2011-06-06 10:06:21 +0000

    Correction de la classe Set : - suppression de la méthode size() qui faisait doublon avec length() et créait un bug caché dans la méthode add - correction de la méthode add : size modifié en sz

--- src/Set.h
+++ src/Set.h
@@ -1,32 +1,34 @@
 /*
- * Ensemble.h
+ * Set.h
  *
  *  Created on: 31 mai 2011
- *      Author: quentin
+ *      Author: quentin, Michelet Valentin
  */
 
 #ifndef SET_H_
 #define SET_H_
 
-template <class T>
-class Set
-{
+template<class T>
+class Set {
 protected:
 	int sz;
 	T* data;
 	int used;
+
 public:
-	Set ( int _size= 4 );
+	Set(int _size = 4);
 	virtual ~Set();
 
-	bool add ( T item ); // always added to the end
-	bool truncate (); // make size = used
-	void clear ();
-	int length ();
-	int size ();
-	bool empty ();
-	//const T& operator[] (int i);
-	T get (int i) const;
+	bool add(T item); // always added to the end
+	bool truncate(); // make size = used
+	void clear();
+	int length();
+	bool empty();
+
+	T operator[](int i) const;
+	T& operator[](const int &i);
+
+	T get(int i) const;
 
 };
 #include "Set.hpp"
