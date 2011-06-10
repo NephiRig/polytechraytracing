a04b4db23520ea949b48b772a24edb30a4737daa
delicesdargan <delicesdargan@gmail.com>
2011-06-10 21:14:16 +0000

    classe ParamSurf.cpp

--- src/ParamSurf.cpp
+++ src/ParamSurf.cpp
@@ -4,14 +4,18 @@
  *  Created on: 31 mai 2011
  *      Author: Marion & Sandra
  */
-/*
+
 #include "ParamSurf.h"
 
 
 extern "C"{
-void zggev_(char *jobvl, char *jobvr, int *n, double *a, int *lda, double *b, int *ldb, double *alpha,double *beta, double *vl, int *ldvl, double *vr, int *ldvr,double *work, int *lwork, double *rwork, int *info);
+/*void dggev_(char *jobvl, char *jobvr, int *n, double *a, int *lda, double *b, int *ldb, double *alpha,double *beta, double *vl, int *ldvl, double *vr, int *ldvr,double *work, int *lwork, double *rwork, int *info);*/
+	void dggev_(const char* JOBVL, const char* JOBVR, const int* N,
+                       const double* A, const int* LDA, const double* B, const int* LDB,
+                       double* ALPHAR, double* ALPHAI, double* BETA,
+                       double* VL, const int* LDVL, double* VR, const int* LDVR,
+                       double* WORK, const int* LWORK, int* INFO);
 }
-
 
 ParamSurf::ParamSurf(const Color &_color,mat M0, mat M1, mat M2, mat M3) : 
 	 M0(M0), M1(M1), M2(M2), M3(M3) {
@@ -22,9 +26,9 @@ ParamSurf::ParamSurf(const Color &_color,mat M0, mat M1, mat M2, mat M3) :
 ParamSurf::~ParamSurf() {
 }
 
-/ *
+/*
  *	Calculate the pencil of a parametric surface
- * /
+ */
 Set<mat> ParamSurf::make_pencil(const Ray& r) const {
 
 
@@ -41,10 +45,10 @@ Set<mat> ParamSurf::make_pencil(const Ray& r) const {
 	double yd = directionRay.get_y();
 	double zd = directionRay.get_z();
 
-	
-	mat A = xd*M1+yd*M2+zd*M3;
-	mat B = x0*M1+y0*M2+z0*M3+M0;
-/ *
+	mat A = x0*M1+y0*M2+z0*M3+M0;
+	mat B = (-xd)*M1+(-yd)*M2+(-zd)*M3;
+
+/*
 	double [][] A;
 	double [][] B;
 
@@ -54,28 +58,25 @@ Set<mat> ParamSurf::make_pencil(const Ray& r) const {
 			B[i][j] =  M1[i][j]*x0+M2[i][j]*y0+M3[i][j]*z0+M0[i][j];
 		}
 	}
-* /
+*/
 	pencil.add(A);
 	pencil.add(B);
 
 	return pencil;
 }
 
-/ *
+/*
  *	Calculate the intersections of a ray with a parametric surface
- * /
+ */
 Set<double> ParamSurf::intersect(const Ray& r) const {
 
 	Set<mat> pencil = make_pencil(r);
 
-	mat A = pencil.get(1);
-	mat B = pencil.get(2);
+	mat A = pencil.get(0);
+	mat B = pencil.get(1);
+	//cout << "A = " << A << endl;
+	//cout << "B = " << B << endl;
 
-	cout << "A = " << A << endl;
-	cout << "B = " << B << endl;
-
-	vec lambda;
-	mat evecs;
 	char jobvl = 'N', jobvr = 'V';
 	int n, lda, ldb, ldvl, ldvr, lwork, info;
 	n=lda=A.rows();
@@ -85,24 +86,23 @@ Set<double> ParamSurf::intersect(const Ray& r) const {
 	lwork = std::max(1,  n*n+64); // This may be choosen better! ???
 
 	vec work(lwork);
-	vec rwork(8*n); // This may be choosen better
-	vec alpha(n), beta(n);
-	mat vl(1,1), vr(n, n);
-	zggev_(&jobvl, &jobvr, &n, A._data(), &lda,
-        B._data(), &ldb, alpha._data(),  beta._data(), vl._data(),
-        &ldvl, vr._data(), &ldvr, work._data(), &lwork,
-        rwork._data(), &info);
-	lambda=elem_div(alpha,beta);
-	cout<<endl;
-	cout<< "eig(A,B)= \n";
+	vec alphar(n), alphai(n), beta(n);
+	mat vl(1,n), vr(n, n);
+	dggev_(&jobvl, &jobvr, &n, A._data(), &lda,
+        B._data(), &ldb, alphar._data(), alphai._data(), beta._data(), vl._data(),
+        &ldvl, vr._data(), &ldvr, work._data(), &lwork, &info);
 
-	Set<double> gev = Set<double>(6);
-	for(int i=0;sizeof(lambda);i++){
-		gev.add(lambda(i));
+	//cout << "alphar = " << alphar << endl;
+	//cout << "alphai = " << alphai << endl;
+	//cout << "beta = " << beta << endl;
+
+	Set<double> gev = Set<double>(n);
+	for(int i=0; i < n; i++){
+		if (beta(i)!=0 && beta(i) > abs(0.00001)) {
+			gev.add(alphar(i)/beta(i));
+		}
 	}
 
-
-	cout << "lambda = "<< lambda <<endl;
 	return gev;
 }
 
@@ -111,14 +111,19 @@ Vector3 ParamSurf::normal(const Vector3& pt_intersection) const {
 }
 
 
-int main(){
-	/ *mat M0 = "-1,0,0,0,0,-2; 0,-2,0,0,0,0; 0,0,0,0,-2,0; 0,0,0,0,0,0; 0,0,0,0,0,0; -4,0,0,0,-8,-8";
+int main2(){
+	mat M0 = "-1,0,0,0,0,-2; 0,-2,0,0,0,0; 0,0,0,0,-2,0; 0,0,0,0,0,0; 0,0,0,0,0,0; -4,0,0,0,-8,-8";
 	mat M1 = "-1,0,1,0,0,-1; 0,0,0,0,0,0; 0,0,0,0,0,0; 0,0,0,0,0,0; 0,0,0,0,0,0; -4,0,0,0,0,-4";
 	mat M2 = "0,0,0,0,0,0; -1,0,1,0,0,-1; 0,0,0,0,0,0; 0,0,0,0,0,-2; 0,0,0,-2,0,0; 0,4,0,0,0,0";
 	mat M3 = "0,0,0,0,0,0; 0,0,0,0,0,0; -1,0,1,0,0,-1; 0,0,0,2,0,0; 2,0,0,0,0,0; 0,0,0,0,6,0";
 	
-	ParamSurf* paramSurf1 = new ParamSurf(Color(0,1,0),M0,M1,M2,M3);
-	Vector3 origin= Vector3(2,1,2) const Vector3 &direction
-	paramSurf1.intersect(* /
+	ParamSurf paramSurf1 = ParamSurf(Color(0,1,0),M0,M1,M2,M3);
+	Vector3 origin= Vector3(0,0,0);
+	Vector3 direction= Vector3(2,1,2);cout << "priut"<< endl;
+	Set<double> gev =paramSurf1.intersect(Ray(origin,direction));
+
+	cout << "gen vp " << endl;
+	for(int i=0;i<gev.size();i++){
+		cout << gev.get(i) << endl;
+	}
 }
-*/
