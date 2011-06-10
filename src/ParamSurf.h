9f3d486a50750da007490d52b91e9ac5b6707b41
marion.ferrara.w <marion.ferrara.w@gmail.com>
2011-06-10 18:26:35 +0000

    ParamSurf.h works

--- src/ParamSurf.h
+++ src/ParamSurf.h
@@ -7,7 +7,7 @@
 
 #ifndef PARAMSURF_H_
 #define PARAMSURF_H_
-/*
+
 #include "Shape.h"
 #include "Material.h"
 #include "Vector3.h"
@@ -26,7 +26,7 @@ public:
 	Color color;
 	mat M0, M1, M2, M3;
 
-	ParamSurf (const Color &_color, mat M0, mat M1, mat M2, mat M3);
+	ParamSurf (const Color &_color, mat M0, mat M1, mat M2, mat M3 /*Vector3 normal*/);
 	virtual ~ParamSurf();
 
 	Vector3 normal (const Vector3& pt) const;
@@ -36,5 +36,5 @@ public:
 	Set<mat> make_pencil (const Ray& r) const;
 
 };
-*/
+
 #endif /* PARAMSURF_H_ */
