d4bcda8875b932c2d501a15c5345ebe8cddb9479
shaolan60 <shaolan60@gmail.com>
2011-06-14 13:52:50 +0000

    Damier sous forme de plan Bientôt un vrai damier ! oupas...

--- src/Draughtboard.cpp
+++ src/Draughtboard.cpp
@@ -12,11 +12,11 @@
 using namespace std;
 
 //*
-Draughtboard::Draughtboard(const Color &color, const Color &colorD, const Ray &normAndPoint, double length, double width, double caseSize) :
-	_colorD(colorD), _normAndPoint(normAndPoint), _length(length), _width(width), _caseSize(caseSize) {
+Draughtboard::Draughtboard(const Color &color, /*const Color &colorD,*/ const Ray &normAndPoint, double height, double width/*, double caseSize*/) :
+	/*_colorD(colorD), */_normAndPoint(normAndPoint), _height(height), _width(width)/*, _caseSize(caseSize)*/ {
 	this->color = color;
 	//Calculs de la constante d dans l'équation du plan P:ax+by+cz=d
-	_d = _normAndPoint.get_direction()[0] * _normAndPoint.get_origin()[0] + _normAndPoint.get_direction()[1] * _normAndPoint.get_origin()[1] + _normAndPoint.get_direction()[2] * _normAndPoint.get_origin()[2];
+	_d = _normAndPoint[0][0] * _normAndPoint[1][0] + _normAndPoint[0][1] * _normAndPoint[1][1] + _normAndPoint[0][2] * _normAndPoint[1][2];
 }
 
 Draughtboard::~Draughtboard() {
@@ -24,8 +24,9 @@ Draughtboard::~Draughtboard() {
 }
 
 Set<double> Draughtboard::intersect(const Ray& r) const {
-	Set<double> res = Set<double>();
-	double t = (_d - _normAndPoint[1][0] * r[0][0] - _normAndPoint[1][1] * r[0][1] - _normAndPoint[1][2] * r[0][2]) / (r[1][0] + r[1][1] + r[1][2]);
+	Set<double> res = Set<double>(1);
+	double t = (_d - _normAndPoint[1][0]*r[0][0] - _normAndPoint[1][1]*r[0][1] - _normAndPoint[1][2]*r[0][2])/(_normAndPoint[1][0]*r[1][0] + _normAndPoint[1][1]*r[1][1] + _normAndPoint[1][2]*r[1][2]);
+
 	if (t > 0)
 		res.add(t);
 	return res;
@@ -40,9 +41,9 @@ Color Draughtboard::get_color(const Vector3 &pt) {
 	if ((int) floor((pt / _caseSize)[0]) % 2 == (int) floor((pt / _caseSize)[1]) % 2)
 		return color;
 	else//*/
-	if (pt[0] > 0 && pt[1] > 0)
+	/*if (pt[0] > 0 && pt[1] > 0)
 		return color;
-	else
-		return _colorD;
+	else//*/
+		return color;
 }
 //*/
--- src/Draughtboard.h
+++ src/Draughtboard.h
@@ -15,22 +15,20 @@
 //*
 class Draughtboard: public Shape {
 public:
-	Color _colorD;
+	//Color _colorD;
 	Ray _normAndPoint;
 	double _d;
-	double _length;
+	double _height;
 	double _width;
-	double _caseSize;
+	//double _caseSize;
 
 	Draughtboard() {};
-	Draughtboard(const Color &color, const Color &colorD, const Ray &normAndPoint, double length,
-			double width, double caseSize);
+	Draughtboard(const Color &color, /*const Color &colorD,*/ const Ray &normAndPoint, double height,
+			double width/*, double caseSize*/);
 	~Draughtboard();
 
 	Set<double> intersect(const Ray& r) const;
-
 	Vector3 normal(const Vector3& pt) const;
-
 	Color get_color(const Vector3 &pt);
 };
 //*/
