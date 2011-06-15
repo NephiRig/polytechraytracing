3029066f0a3f909d4f5ec97a5a7af0586af4a3c8
quentin.grimaud@gmail.com <quentin.grimaud@gmail.com@f53c08bf-0994-eb6e-57c7-fd235582c9a5>
2011-06-15 13:41:35 +0000

    fixes for forgotten normalizations

--- trunk/src/Rectangle.cpp
+++ trunk/src/Rectangle.cpp
@@ -26,7 +26,7 @@ Set<double> Rectangle::intersect(const Ray& r) const {
 	Vector3 inter = r.getPoint(t);
 	if ((fabs(inter[0]-_normAndPoint[0][0]) < _width/2.0 && fabs(inter[2]-_normAndPoint[0][2]) < _height/2.0) && t > 0) {
 		res.add(t);
-		cout << t << " " << r.getPoint(t) << endl;
+		//cout << t << " " << r.getPoint(t) << endl;
 	}
 	return res;
 }
