8c055bbe1a7a4e2b7364ba0a11f4489322df0d98
quentin.grimaud@gmail.com <quentin.grimaud@gmail.com@f53c08bf-0994-eb6e-57c7-fd235582c9a5>
2011-06-15 19:48:44 +0000

    updated createvideo.sh to handle gif format (gives sometimes ugly colors but is better for looping animations)

--- trunk/createvideo.sh
+++ trunk/createvideo.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 # http://electron.mit.edu/~gsteele/ffmpeg/
-for f in *ppm ; do convert -quality 100 $f `basename $f ppm`jpg; done 
+# http://www.ehow.com/how_7211144_make-gif-animation-linux.html
+echo "Converting ppm images to jpg"
+for f in *ppm ; do convert -quality 100 $f `basename $f ppm`jpg; done
+echo "Creating video from jpg sequence"
 mencoder "mf://*.jpg" -mf fps=25 -o video.avi -ovc lavc -lavcopts vcodec=msmpeg4v2:vbitrate=8000
+echo "Creating gif from jpg sequence"
+convert -delay 4 -loop 0 *.jpg video.gif
+echo "Done"
