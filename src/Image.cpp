9383f1737539ac7a61594117ddaab44c734948a5
shaolan60 <shaolan60@gmail.com>
2011-06-22 14:22:11 +0000

    Modification de la classe Image : - suppression de la méthode safeSetPixel, inutilisée et alias de la méthode setPixel - ajout d'un constructeur à partir d'une image existante - modification des noms de variables de classe - ajout de la méthode printImage pour enregistrer une image avec un nom donné en paramètre

--- src/Image.cpp
+++ src/Image.cpp
@@ -5,77 +5,88 @@
 #include <iostream> // debug
 #include <cassert>
 
-Image::Image(int _w, int _h) :
-	w(_w), h(_h) {
-	data = new Color*[w];
-	for (int i = 0; i < w; i++) {
-		data[i] = new Color[h];
+Image::Image(int w, int h) :
+	_w(w), _h(h) {
+	this->_data = new Color*[this->_w];
+	for (int x = 0; x < this->_w; x++) {
+		this->_data[x] = new Color[this->_h];
 	}
 }
 
-Image::Image(int _w, int _h, Color c) {
-	this->w = _w;
-	this->h = _h;
-	assert (this->w > 0 && this->h > 0);
-	this->data = new Color*[this->w];
-	assert(this->data != 0);
-	for (int i = 0; i < this->w; i++) {
-		this->data[i] = new Color[this->h];
-		assert(data[i]);
-		for (int j = 0; j < this->h; j++) {
-			this->data[i][j] = c;
+Image::Image(int w, int h, Color c) {
+	this->_w = w;
+	this->_h = h;
+	assert (this->_w > 0 && this->_h > 0);
+
+	this->_data = new Color*[this->_w];
+	assert(this->_data != 0);
+
+	for (int x = 0; x < this->_w; x++) {
+		this->_data[x] = new Color[this->_h];
+		assert(_data[x]);
+		for (int y = 0; y < this->_h; y++) {
+			this->_data[x][y] = c;
+		}
+	}
+}
+
+Image::Image(const Image *source) {
+	this->_w = source->_w;
+	this->_h = source->_h;
+
+	this->_data = new Color*[this->_w];
+	for (int x = 0; x < this->_w; x++) {
+		this->_data[x] = new Color[this->_h];
+		for (int y = 0; y < this->_h; y++) {
+			this->_data[x][y] = source->getPixel(x, y);
 		}
 	}
 }
 
 int Image::width() const {
-	return w;
+	return this->_w;
 }
 
 int Image::height() const {
-	return h;
-}
-
-void Image::safeSetPixel(int x, int y, Color c) {
-	if (x >= 0 && x < w && y >= 0 && y < h)
-		data[x][y] = c;
+	return this->_h;
 }
 
 void Image::setPixel(int x, int y, Color c) {
-	if (x >= 0 && x < w && y >= 0 && y < h) {
-		data[x][y] = c;
+	if (x >= 0 && x < this->_w && y >= 0 && y < this->_h) {
+		this->_data[x][y] = c;
 	}
 }
 
 Color Image::getPixel(int x, int y) const {
 	x = (x >= 0 ? x : 0);
-	x = (x < w ? x : w - 1);
+	x = (x < this->_w ? x : this->_w - 1);
 	y = (y >= 0 ? y : 0);
-	y = (y < h ? y : h - 1);
-	return data[x][y];
+	y = (y < this->_h ? y : this->_h - 1);
+	return this->_data[x][y];
 }
 
 void Image::writePPM(std::ostream &s) const {
-	s << "P6\n" << w << " " << h << "\n255\n";
+	s << "P6\n" << this->_w << " " << this->_h << "\n255\n";
 	unsigned int i;
-	for (int y = 0; y < h; y++)
-		for (int x = 0; x < w; x++) {
+	for (int y = 0; y < this->_h; y++) {
+		for (int x = 0; x < _w; x++) {
 			double gamma = 1.0 / 2.2;
-			data[x][y] = Color(pow(data[x][y][0], gamma), pow(data[x][y][1],
-					gamma), pow(data[x][y][2], gamma));
-			i = (unsigned int) (256.0 * data[x][y][0]);
+			this->_data[x][y] = Color(pow(this->_data[x][y][0], gamma), pow(this->_data[x][y][1],
+					gamma), pow(this->_data[x][y][2], gamma));
+			i = (unsigned int) (256.0 * this->_data[x][y][0]);
 			if (i > 255)
 				i = 255;
 			s.put((unsigned char) i);
-			i = (unsigned int) (256.0 * data[x][y][1]);
+			i = (unsigned int) (256.0 * this->_data[x][y][1]);
 			if (i > 255)
 				i = 255;
 			s.put((unsigned char) i);
-			i = (unsigned int) (256.0 * data[x][y][2]);
+			i = (unsigned int) (256.0 * this->_data[x][y][2]);
 			if (i > 255)
 				i = 255;
 			s.put((unsigned char) i);
 		}
+	}
 }
 
 void Image::readPPM(std::string file_name) {
@@ -88,31 +99,39 @@ void Image::readPPM(std::string file_name) {
 
 	char ch, type;
 	char red, green, blue;
-	int i, j, cols, rows;
+	int x, y, cols, rows;
 	int num;
 
 	in.get(ch);
 	in.get(type);
 	in >> cols >> rows >> num;
 
-	w = cols;
-	h = rows;
+	this->_w = cols;
+	this->_h = rows;
 
-	data = new Color*[w];
-	for (i = 0; i < w; i++)
-		data[i] = new Color[h];
+	this->_data = new Color*[this->_w];
+	for (x = 0; x < this->_w; x++) {
+		this->_data[x] = new Color[this->_h];
+	}
 
 	in.get(ch);
 
-	for (i = h-1; i >= 0; i--) {
-		for (j = 0; j < w; j++) {
+	for (int y = 0; y < this->_h; y++) {
+		for (int x = 0; x < this->_w; x++) {
 			in.get(red);
 			in.get(green);
 			in.get(blue);
-			data[i][j] = Color(
+			this->_data[x][y] = Color(
 					(float) ((unsigned char)red)/255.0,
 					(float) ((unsigned char)green)/255.0,
 					(float) ((unsigned char)blue)/255.0);
 		}
 	}
+}
+
+void Image::printImage(const char* fileName) const {
+	std::ofstream myfile;
+	myfile.open(fileName);
+	this->writePPM(myfile);
+	myfile.close();
 }
