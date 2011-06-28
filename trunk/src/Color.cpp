c8c5e6bebb6083078c55e4581a48889b8df562e1
shaolan60@gmail.com <shaolan60@gmail.com@f53c08bf-0994-eb6e-57c7-fd235582c9a5>
2011-06-28 16:26:46 +0000

    Ajout d'une doc pour la classe Color

--- trunk/src/Color.cpp
+++ trunk/src/Color.cpp
@@ -2,56 +2,56 @@
 #include <stdint.h>
 
 Color::Color(double r/*= 0.0*/, double g/*= 0.0*/, double b/*= 0.0*/) {
-	val[0] = r;
-	val[1] = g;
-	val[2] = b;
+	_val[0] = r;
+	_val[1] = g;
+	_val[2] = b;
 }
 
-Color::Color(const Color &c) {
-	*this = c;
+Color::Color(const Color &color) {
+	*this = color;
 }
 
 Color::~Color() {
 }
 
 double Color::getR() const {
-	return val[0];
+	return _val[0];
 }
 
 double Color::getG() const {
-	return val[1];
+	return _val[1];
 }
 
 double Color::getB() const {
-	return val[2];
+	return _val[2];
 }
 
 void Color::setR(double r) {
-	val[0] = r;
+	_val[0] = r;
 }
 
 void Color::setG(double g) {
-	val[1] = g;
+	_val[1] = g;
 }
 
 void Color::setB(double b) {
-	val[2] = b;
+	_val[2] = b;
 }
 
 double& Color::operator[](unsigned i) {
-	return val[i];
+	return _val[i];
 }
 
 double Color::operator[](unsigned i) const {
-	return val[i];
+	return _val[i];
 }
 
-bool operator==(const Color &c1, const Color &c2) {
-	return c1[0] == c2[0] && c1[1] == c2[1] && c1[2] == c2[2];
+bool operator==(const Color &color1, const Color &color2) {
+	return color1[0] == color2[0] && color1[1] == color2[1] && color1[2] == color2[2];
 }
 
-bool operator!=(const Color &c1, const Color &c2) {
-	return !(c1 == c2);
+bool operator!=(const Color &color1, const Color &color2) {
+	return !(color1 == color2);
 }
 
 const Color& Color::operator+() const {
@@ -59,50 +59,50 @@ const Color& Color::operator+() const {
 }
 
 Color Color::operator-() const {
-	return Color(-val[0], -val[1], -val[2]);
+	return Color(-_val[0], -_val[1], -_val[2]);
 }
 
-Color& Color::operator=(const Color &c) {
-	val[0] = c[0];
-	val[1] = c[1];
-	val[2] = c[2];
+Color& Color::operator=(const Color &color) {
+	_val[0] = color[0];
+	_val[1] = color[1];
+	_val[2] = color[2];
 	return *this;
 }
 
-Color operator+(const Color &c1, const Color &c2) {
-	return Color(c1[0] + c2[0], c1[1] + c2[1], c1[2] + c2[2]);
+Color operator+(const Color &color1, const Color &color2) {
+	return Color(color1[0] + color2[0], color1[1] + color2[1], color1[2] + color2[2]);
 }
 
-Color operator*(const Color &c1, const Color &c2) {
-	return Color(c1[0] * c2[0], c1[1] * c2[1], c1[2] * c2[2]);
+Color operator*(const Color &color1, const Color &color2) {
+	return Color(color1[0] * color2[0], color1[1] * color2[1], color1[2] * color2[2]);
 }
 
-Color operator/(const Color &c1, const Color &c2) {
-	return Color(c1[0] / c2[0], c1[1] / c2[1], c1[2] / c2[2]);
+Color operator/(const Color &color1, const Color &color2) {
+	return Color(color1[0] / color2[0], color1[1] / color2[1], color1[2] / color2[2]);
 }
 
-Color operator*(const double k, const Color &c) {
-	return Color(c[0] * k, c[1] * k, c[2] * k);
+Color operator*(const double k, const Color &color) {
+	return Color(color[0] * k, color[1] * k, color[2] * k);
 }
 
-Color operator*(const Color &c, const double k) {
-	return Color(c[0] * k, c[1] * k, c[2] * k);
+Color operator*(const Color &color, const double k) {
+	return Color(color[0] * k, color[1] * k, color[2] * k);
 }
 
-Color operator/(const Color &c, const double k) {
-	return Color(c[0] / k, c[1] / k, c[2] / k);
+Color operator/(const Color &color, const double k) {
+	return Color(color[0] / k, color[1] / k, color[2] / k);
 }
 
-Color& Color::operator+=(const Color &c) {
-	return *this = *this + c;
+Color& Color::operator+=(const Color &color) {
+	return *this = *this + color;
 }
 
-Color& Color::operator*=(const Color &c) {
-	return *this = *this * c;
+Color& Color::operator*=(const Color &color) {
+	return *this = *this * color;
 }
 
-Color& Color::operator/=(const Color &c) {
-	return *this = *this / c;
+Color& Color::operator/=(const Color &color) {
+	return *this = *this / color;
 }
 
 Color& Color::operator*=(const double k) {
@@ -114,23 +114,23 @@ Color& Color::operator/=(const double k) {
 }
 
 void Color::clamp() {
-	if (val[0] < 0)
-		val[0] = 0;
-	if (val[1] < 0)
-		val[1] = 0;
-	if (val[2] < 0)
-		val[2] = 0;
+	if (_val[0] < 0)
+		_val[0] = 0;
+	if (_val[1] < 0)
+		_val[1] = 0;
+	if (_val[2] < 0)
+		_val[2] = 0;
 
-	if (val[0] > 1)
-		val[0] = 1;
-	if (val[1] > 1)
-		val[1] = 1;
-	if (val[2] > 1)
-		val[2] = 1;
+	if (_val[0] > 1)
+		_val[0] = 1;
+	if (_val[1] > 1)
+		_val[1] = 1;
+	if (_val[2] > 1)
+		_val[2] = 1;
 }
 
-std::ostream& operator<<(std::ostream &os, const Color &c) {
-	os << "rgb(" << c[0] << ", " << c[1] << ", " << c[2] << ")";
+std::ostream& operator<<(std::ostream &os, const Color &color) {
+	os << "rgb(" << color[0] << ", " << color[1] << ", " << color[2] << ")";
 	return os;
 }
 
@@ -140,9 +140,9 @@ std::ostream& operator<<(std::ostream &os, const Color &c) {
  uint32_t Color::getRGB () const
  {
  uint32_t res = 0;
- res |= (int(val[0]*255) & 0xff) << 16;
- res |= (int(val[1]*255) & 0xff) << 8;
- res |= (int(val[2]*255) & 0xff);
+ res |= (int(_val[0]*255) & 0xff) << 16;
+ res |= (int(_val[1]*255) & 0xff) << 8;
+ res |= (int(_val[2]*255) & 0xff);
  return res;
  }
  */
