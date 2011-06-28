fd924590d51d4931bb3375297bff4edeabdd41f9
shaolan60 <shaolan60@gmail.com>
2011-06-28 13:52:13 +0000

    Éclatement de la méthode parser() avec appel de sous-fonctions + documentation doxygen du .h

--- src/SceneParser.h
+++ src/SceneParser.h
@@ -1,6 +1,17 @@
 #ifndef SCENEPARSER_H_
 #define SCENEPARSER_H_
 
+/**
+ * \file SceneParser.h
+ * \brief XML file parser
+ * \author Michelet Valentin
+ * \version 0.7
+ * \date June 2011
+ *
+ * Parse scene description XML files.
+ * From a well written scene XML file, every shape, light source and screen option are initialized.
+ * To write such a file, please see the tutorial "How to describe a scene".
+ */
 #include <string>
 
 #include "tinyxml.h"
@@ -8,6 +19,8 @@
 #include "Draughtboard.h"
 #include "ImageTexture.h"
 #include "LightSource.h"
+#include "MarbleTexture.h"
+#include "NoiseTexture.h"
 #include "Plane.h"
 #include "Rectangle.h"
 #include "ScreenV2.h"
@@ -18,6 +31,12 @@
 #include "Texture.h"
 #include "UVSphere.h"
 
+/**
+ * \class SceneParser
+ * \brief XML file parser class
+ *
+ * Class to parse scene description XML files.
+ */
 class SceneParser {
 public:
 	char* _fileName;
@@ -27,10 +46,63 @@ public:
 	//FIXME ajouter la caméra
 	//ScreenV2 _camera;
 
+	/**
+	 * \brief Constructor
+	 *
+	 * Construct the parser from a XML file.
+	 */
 	SceneParser(char* fileName);
+
+	/**
+	 * \brief Destructor
+	 *
+	 * Destruct the parser.
+	 */
 	virtual ~SceneParser();
 
-	int parse();
+	/**
+	 * \brief general parser
+	 *
+	 * Parse the XML file specified by the class instance _fileName
+	 */
+	void parse();
+
+	/**
+	 * \brief screen setup
+	 *
+	 * Parse the XML file to set the screen options.
+	 *
+	 * \param elem : the node that contain the screen options
+	 */
+	void screenSetup(TiXmlElement *elem);
+
+	/**
+	 * \brief shape parser
+	 *
+	 * Parse the XML file to set the different shapes.
+	 *
+	 * \param elem : the node that contain the shapes
+	 */
+	void parseShape(TiXmlElement *elem);
+
+	/**
+	 * \brief light parser
+	 *
+	 * Parse the XML file to set the different lights.
+	 *
+	 * \param elem : the node that contain the lights
+	 */
+	void parseLight(TiXmlElement *elem);
+
+	/**
+	 * \brief texture parser
+	 *
+	 * Parse the XML file to get a texture.
+	 *
+	 * \param elem : the node that contain the texture
+	 * \return the texture parsing
+	 */
+	Texture* parseTexture(TiXmlElement *elem);
 };
 
 #endif /* SCENEPARSER_H_ */
